ef57a4f89a10a1f2cc7f842c9100c764c042def8
Batuh <200205011@ostimteknik.edu.tr>
2023-12-14 15:46:54 +0300

    TestVer

---
+++ Assets/Scripts/SignLanguageIsTrue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignLanguageIsTrue : MonoBehaviour
+{
+    
+    //private void Start()
+    //{
+    //    request = GetComponent<WebRequest>();
+    //}
+
+    
+
+    private void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/WebRequest.cs
+++ Assets/Scripts/WebRequest.cs
@@ -5,9 +5,26 @@ using Unity.VisualScripting;
 
 public class WebRequest : MonoBehaviour
 {
+    [SerializeField] public string[] text;
+    [SerializeField] GameObject[] HandSign;
+    string[] SignAlphabet = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "S", "U", "V", "W", "X", "Y" };
+    string sign;
+    bool isTrue;
+    float fTime = 5;
+
     private void Update()
     {
+        fTime -= Time.deltaTime;
+        if (fTime <= 0)
+        {
         StartCoroutine(GetRequest("http://127.0.0.1:5000/predict"));
+
+        if (sign == text[1])
+            {
+               OnClick();
+            }
+            fTime = 5;
+        }
     }
 
     IEnumerator GetRequest(string uri)
@@ -19,9 +36,30 @@ public class WebRequest : MonoBehaviour
 
             string[] pages = uri.Split('/');
             int page = pages.Length - 1;
-
-            Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text);
-
+            text = webRequest.downloadHandler.text.ToString().Split(":");
+            if (text[1] != null) 
+            {
+            Debug.Log(pages[page] + ":\nReceived: " + webRequest.downloadHandler.text + text[1]);
+            }
         }
     }
+
+    [ContextMenu("aaa")]
+    public void OnClick()
+    {
+        foreach (GameObject obj in HandSign)
+        {
+            obj.active = false;
+        }
+        int RastgeleSayi = Random.RandomRange(1, 23);
+        sign = SignAlphabet[RastgeleSayi];
+        foreach (GameObject obj in HandSign)
+        {
+            if (sign == obj.name)
+            {
+                obj.active = true;
+            }
+        }
+    }
+
 }
